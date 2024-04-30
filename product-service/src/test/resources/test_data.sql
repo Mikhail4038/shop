3534981d665846e2c7d9874cda34c5a47272a17b
Mikhail <mihaila4038@gmail.com>
2024-05-01 00:03:17 +0300

    product-service

--- product-service/src/test/resources/test_data.sql
+++ product-service/src/test/resources/test_data.sql
@@ -1,12 +1,12 @@
-insert into t_producer_address (created,modified,street,house,city,country,locale) values(now(),now(),'Kupaly','12','Hrodno','Belarus','BY');
+insert into t_producer_address (created,modified,street,house,city,country,postcode) values(now(),now(),'Kupaly','12','Hrodno','Belarus','230026');
 
-insert into t_producer (created,modified,name,address_id) values(now(),now(),'Conte',1);
+insert into t_producer (created,modified,name,producer_address_id) values(now(),now(),'Conte',1);
 
 insert into t_price(created,modified,cost,is_promotional) values(now(),now(),'65.0',1);
 insert into t_price(created,modified,cost,is_promotional) values(now(),now(),'32.0',0);
 
-insert into t_product(created,modified,ean,name,price_id,expiration_date,producer_id) values(now(),now(),'123','jersey',1,'2023-11-10',1);
-insert into t_product(created,modified,ean,name,price_id,expiration_date,producer_id) values(now(),now(),'456','shorts',2,'2030-11-04',1);
+insert into t_product(created,modified,ean,name,price_id,producer_id) values(now(),now(),'123','jersey',1,1);
+insert into t_product(created,modified,ean,name,price_id,producer_id) values(now(),now(),'456','shorts',2,1);
 
 insert into t_review(created,modified,user_id,message,assessment,product_id) values(now(),now(),1,'Beautiful thing',5,1);
 insert into t_review(created,modified,user_id,message,assessment,product_id) values(now(),now(),1,'A very nice thing',10,2);
